7a344d35ae292e8a991d92c6f53f9d579f2cb6d5
thisisvarun <varunthurram1@gmail.com>
2025-04-11 17:08:23 +0530

    ooo

--- truffle-config.js
+++ truffle-config.js
@@ -1,35 +1,30 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const path = require('path');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  migrations_directory: "./migrations",
   networks: {
     sepolia: {
-      provider: () => new HDWalletProvider({
-        mnemonic: {
-          phrase: fs.readFileSync(path.resolve(__dirname, '.secret'), 'utf8').trim()
-        },
-        providerOrUrl: `https://sepolia.infura.io/v3/98a83f6e77274b438488553bf62ca975`,
-        chainId: 11155111, // Sepolia's network ID
-        gas: 4000000, // Adjust as needed
-        gasPrice: 10000000000, // Adjust as needed
-        confirmations: 2,
-        timeoutBlocks: 200,
-        skipDryRun: true
-      }),
-      network_id: '11155111', // Sepolia's network ID
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://sepolia.infura.io/v3/98a83f6e77274b438488553bf62ca975`
+        ),
+      network_id: 11155111,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-      version: "0.8.0",
+      version: '0.8.0',
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
